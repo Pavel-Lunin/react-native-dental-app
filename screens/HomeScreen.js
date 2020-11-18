103255c7d35732cdbb932be81e5805c053c44426
Pavel-Lunin <62575772+Pavel-Lunin@users.noreply.github.com>
2020-11-19 08:33:44 +1000

    Animated,fixed errors. Add useNativeDriver

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,12 +6,12 @@ import Swipeable from 'react-native-swipeable-row';
 import { appointmentsApi } from '../utils/api';
 
 import { Appointment, SectionTitle, PlusButton } from '../components';
+import Animated from 'react-native-reanimated';
 
 const HomeScreen = ({ route, navigation }) => {
   const { params } = route;
   const [data, setData] = React.useState(null);
   const [isLoading, setIsLoading] = React.useState(false);
-  const [lastUpdateTime, setLastUpdateTime] = React.useState(null);
 
   const fetchAppointents = () => {
     setIsLoading(true);
@@ -65,19 +65,21 @@ const HomeScreen = ({ route, navigation }) => {
           onRefresh={fetchAppointents}
           refreshing={isLoading}
           renderItem={({ item }) => (
-            <Swipeable
-              rightButtons={[
-                <SwipeViewButton style={{ backgroundColor: '#84C1CB' }}>
-                  <Ionicons name="md-create" size={28} color="white" />
-                </SwipeViewButton>,
-                <SwipeViewButton
-                  onPress={removeAppointent.bind(this, item._id)}
-                  style={{ backgroundColor: '#F85A5A' }}>
-                  <Ionicons name="ios-close" size={48} color="white" />
-                </SwipeViewButton>,
-              ]}>
-              <Appointment navigate={navigation.navigate} item={item} />
-            </Swipeable>
+            <Animated.View>
+              <Swipeable
+                rightButtons={[
+                  <SwipeViewButton style={{ backgroundColor: '#84C1CB' }}>
+                    <Ionicons name="md-create" size={28} color="white" />
+                  </SwipeViewButton>,
+                  <SwipeViewButton
+                    onPress={removeAppointent.bind(this, item._id)}
+                    style={{ backgroundColor: '#F85A5A' }}>
+                    <Ionicons name="ios-close" size={48} color="white" />
+                  </SwipeViewButton>,
+                ]}>
+                <Appointment navigate={navigation.navigate} item={item} />
+              </Swipeable>
+            </Animated.View>
           )}
           renderSectionHeader={({ section: { title } }) => <SectionTitle>{title}</SectionTitle>}
         />
